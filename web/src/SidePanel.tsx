69c535bcf502f450786eae1cd4700f41ad092475
prempreetbrar <prempreet.brar@ucalgary.ca>
2022-11-19 19:34:38 -0700

    [frontend-start] Commit before merging

--- web/src/SidePanel.tsx
+++ web/src/SidePanel.tsx
@@ -19,28 +19,12 @@ import {
   AddCircleOutline,
 } from "@mui/icons-material";
 
+import SearchFieldChip from "./SearchFieldChip";
+
 export default function SidePanel() {
   return (
     <Box display="flex" flexDirection="column" style={{ letterSpacing: "1px" }}>
-      <Paper
-        component="form"
-        sx={{
-          p: "2px 4px",
-          display: "flex",
-          alignItems: "center",
-          width: 200,
-          marginBottom: "20px",
-        }}
-      >
-        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
-          <Search />
-        </IconButton>
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Search Events"
-          inputProps={{ "aria-label": "search google maps" }}
-        />
-      </Paper>
+      <SearchFieldChip />
       <Box
         style={{
           display: "flex",
