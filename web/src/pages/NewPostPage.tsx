4ca290806f7f22ee9aa9112ab5ff4578c81b7b97
Tyler Chen <chentyler52@gmail.com>
2022-11-20 03:18:25 -0700

    Allows for syntax highlighting and autocompletion

--- web/src/pages/NewPostPage.tsx
+++ web/src/pages/NewPostPage.tsx
@@ -3,10 +3,14 @@ import MainLayout from "../components/MainLayout";
 import NewPost from "../components/NewPost";
 
 export const NewPostPage = (props: any) => {
-    return(
-        // <MainLayout children={<NewPost callback={() => {console.log("success!")}}/>}/>
-        <MainLayout>
-            <NewPost callback={() => {console.log("success!")}}/>
-        </MainLayout>
-    )
+  return (
+    // <MainLayout children={<NewPost callback={() => {console.log("success!")}}/>}/>
+    <MainLayout>
+      <NewPost
+        callback={() => {
+          console.log("success!");
+        }}
+      />
+    </MainLayout>
+  );
 };
