256ca552330f9f6a5657457ee8b431967d2c87fd
Mohammad <mo@mohammad.dev>
2022-11-19 20:53:15 -0700

    make things prett

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,12 @@
-// @ts-nocheck
-
 import React from "react";
-import "./App.css";
 import Post from "./Post";
 import SidePanel from "./SidePanel";
 import Navbar from "./Navbar";
 import MainPage from "./MainPage";
 
 import { Box, Typography, IconButton, Grid } from "@mui/material";
+
+import { styled } from "@mui/system";
 
 import { AccountCircle } from "@mui/icons-material";
 import { Route, Routes, Navigate } from "react-router-dom";
@@ -17,34 +16,59 @@ export default function App() {
   console.log(axios.get("http://localhost:3000/users/all"));
   return (
     <>
-      <Box
-        style={{
-          padding: "25px 30px 30px 50px",
-          backgroundColor: "black",
-        }}
-      >
-        <Navbar />
-        <Grid container>
-          <Grid item xs={12} md={3}>
-            <SidePanel />
+      <Container>
+        <ContainerInner>
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            marginBottom="30px"
+          >
+            <Logo>Justly.</Logo>
+            <IconButton aria-label="fingerprint" style={{ color: "#8891A4" }}>
+              <AccountCircle style={{ fontSize: "45px" }} />
+            </IconButton>
+          </Box>
+          <Grid container>
+            <Grid item xs={12} md={3}>
+              <SidePanel />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Post />
+            </Grid>
+            <Grid item xs={12} md={3}></Grid>
           </Grid>
-          <Grid item xs={12} md={6}>
-            <Routes>
-              <Route path="/" element={<Navigate to="/world-trending" />} />
-              <Route
-                path="/world-trending"
-                element={<MainPage worldTrending={true} />}
-              />
-              <Route
-                path="/new-events"
-                element={<MainPage newEvents={true} />}
-              />
-              <Route path="/new-post" element={<MainPage newPost={true} />} />
-            </Routes>
-          </Grid>
-          <Grid item xs={12} md={3}></Grid>
-        </Grid>
-      </Box>
+        </ContainerInner>
+      </Container>
     </>
   );
 }
+
+const Logo = styled(Typography)`
+  && {
+    font-family: "Poppins", sans-serif;
+    font-weight: 600;
+    font-size: 36px;
+  }
+`
+
+const Container = styled('div')`
+  && {
+    background-image: url("https://ichef.bbci.co.uk/news/976/cpsprodpb/179C0/production/_125840769_hi077252483.jpg");
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;
+  }
+  `
+
+const ContainerInner = styled('div')`
+  && {
+    backdrop-filter: blur(30px);
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 25px 30px 30px 50px;
+    box-sizing: border-box;
+    height: 100%;
+  }
+`
